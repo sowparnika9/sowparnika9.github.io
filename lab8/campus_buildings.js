e5dce55a9cb8263434a8c18b87f91746bfe93aca
Sowparnika <111890737+sowparnika9@users.noreply.github.com>
2026-04-24 14:19:49 -0500

    Add campus buildings data in campus_buildings.js

---
+++ lab8/campus_buildings.js
@@ -0,0 +1,33 @@
+var campus_buildings = {
+"type": "FeatureCollection",
+"features": [
+{
+"type": "Feature",
+"properties": { "NAME": "Andrews Hall" },
+"geometry": {
+"type": "Polygon",
+"coordinates": [[
+[-96.70285, 40.81952],
+[-96.70260, 40.81952],
+[-96.70260, 40.81930],
+[-96.70285, 40.81930],
+[-96.70285, 40.81952]
+]]
+}
+},
+{
+"type": "Feature",
+"properties": { "NAME": "Love Library" },
+"geometry": {
+"type": "Polygon",
+"coordinates": [[
+[-96.70210, 40.81890],
+[-96.70170, 40.81890],
+[-96.70170, 40.81860],
+[-96.70210, 40.81860],
+[-96.70210, 40.81890]
+]]
+}
+}
+]
+}
